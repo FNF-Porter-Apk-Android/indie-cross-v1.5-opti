79537a658c6ce66c5737cc392a34aa2767446e08
glauberb0802 <111463416+glauberb0802@users.noreply.github.com>
2023-09-01 15:30:18 -0300

    Add files via upload

---
+++ assets/preload/scripts/script.lua
@@ -0,0 +1,5 @@
+function onCreate()
+makeLuaText('watermark', "Port by glauber01 Opti by VGPH Mods", 0, 2, 690); -- script by glauber01
+    setTextSize('watermark', 20);
+    addLuaText('watermark');
+end
